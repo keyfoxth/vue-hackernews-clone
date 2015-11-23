37b2effdbaaf54bbcb81f969c42e91a0ed406a67
keyfoxth <keyfoxth-github@outlook.com>
2015-11-23 18:37:09 +0900

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var vue = require('vue-loader')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './static',
+    publicPath: '/static/',
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules|vue\/src|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
+        loader: 'babel?optional[]=runtime&loose=all'
+      }
+    ]
+  }
+}
