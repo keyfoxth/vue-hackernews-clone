dd75cffe19c0472b5275475f3b420bb6f3bdf44e
keyfoxth <keyfoxth-github@outlook.com>
2015-11-24 18:11:32 +0900

    Update main.js

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { domain, fromNow } from './filters'
+import App from './components/App.vue'
+import NewsView from './components/NewsView.vue'
+import ItemView from './components/ItemView.vue'
+import UserView from './components/UserView.vue'
+
+Vue.use(Router)
+
+Vue.filter('fromNow', fromNow)
+Vue.filter('domain', domain)
+
+var router = new Router()
+
+router.map({
+  '/news/:page': {
+    component: NewsView
+  },
+  '/user/:id': {
+    component: UserView
+  },
+  '/item/:id': {
+    component: ItemView
+  }
+})
+
+router.beforeEach(function () {
+  window.scrollTo(0, 0)
+})
+
+router.redirect({
+  '*': '/news/1'
+})
+
+router.start(App, '#app')
